7df416da4e05e64047eb311f37e1d60591719d77
kisbucedli <kisbucedli@gmail.com>
2020-02-17 10:51:01 +0100

    Table added to Main view with Toolbar

--- Test_2/WebContent/controller/Main.controller.js
+++ Test_2/WebContent/controller/Main.controller.js
@@ -3,7 +3,10 @@ sap.ui.define(["demo_app/controller/BaseController","demo_app/util/Formatter"
 	"use strict";
 	return BaseController.extend("demo_app.controller.Main",{
 		formatter:Formatter,
-
+		onToggleInfoToolbar: function(oEvent) {
+			var oTable = this.byId("idProductsTable");
+			oTable.getInfoToolbar().setVisible(!oEvent.getParameter("pressed"));
+		}
 	}
 	);
 	
