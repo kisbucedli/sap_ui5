dcaa3d9afe0951f9ca7e3a5667df010724f774a1
kisbucedli <kisbucedli@gmail.com>
2020-02-17 11:27:26 +0100

    template data added to table

--- Test_2/WebContent/controller/Main.controller.js
+++ Test_2/WebContent/controller/Main.controller.js
@@ -3,6 +3,11 @@ sap.ui.define(["demo_app/controller/BaseController","demo_app/util/Formatter"
 	"use strict";
 	return BaseController.extend("demo_app.controller.Main",{
 		formatter:Formatter,
+		onInit: function() {
+			// set explored app's demo model on this sample
+			var oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock") + "/products.json");
+			this.getView().setModel(oModel);
+		},
 		onToggleInfoToolbar: function(oEvent) {
 			var oTable = this.byId("idProductsTable");
 			oTable.getInfoToolbar().setVisible(!oEvent.getParameter("pressed"));
